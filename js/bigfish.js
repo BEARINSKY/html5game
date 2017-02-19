fd0022032dde636d4572745e305de5eea4bbfc08
bear <359083671@qq.com>
2017-02-19 12:37:42 +0800

    big fish

--- js/bigfish.js
+++ js/bigfish.js
@@ -4,19 +4,54 @@
 var bigFishObj=function () {
     this.x;//横坐标
     this.y;//纵坐标
+    this.angle;//角度
     this.eye=new Image();//眼睛
     this.swim=new Image();//身体
     this.tail=new Image();//尾巴
 };
 bigFishObj.prototype.init=function () {
-    this.x=100;
-    this.y=100;
+    this.x=canWidth*0.5;
+    this.y=canHeight*0.5;
+    this.angle=0;
     this.eye.src="./src/bigEye0.png";
     this.swim.src="./src/bigSwim0.png";
     this.tail.src="./src/bigTail0.png";
 };
 bigFishObj.prototype.draw=function () {
-    ctx1.drawImage(this.eye,this.x-this.eye.width*0.5,this.y-this.eye.height*0.5);
-    ctx1.drawImage(this.swim,this.x-this.swim.width*0.5,this.y-this.swim.height*0.5);
-    ctx1.drawImage(this.tail,this.x-this.tail.width*0.5+30,this.y-this.tail.height*0.5);
+    //鼠标移入画布再执行
+    if(mx!=undefined&&my!=undefined)
+    {
+        this.x=lerpD(mx,this.x,0.96);
+        this.y=lerpD(my,this.y,0.96);
+    }
+
+    //计算角度
+    var deltaY=my-this.y;
+    var deltaX=mx-this.x;
+    var beta=Math.atan2(deltaY,deltaX)+Math.PI;
+    this.angle=lerpA(beta,this.angle,0.5);
+
+    ctx1.save();
+    ctx1.translate(this.x,this.y);//将原点定位到此
+    ctx1.rotate(this.angle);//设置角度
+    ctx1.drawImage(this.eye,-this.eye.width*0.5,-this.eye.height*0.5);
+    ctx1.drawImage(this.swim,-this.swim.width*0.5,-this.swim.height*0.5);
+    ctx1.drawImage(this.tail,-this.tail.width*0.5+30,-this.tail.height*0.5);
+    ctx1.restore();
 };
+//设置移动方法，三个参数 目标数值，当前数值，变化率%，返回（当前数值+差值*百分比变化率）
+function lerpD(aim,cur,spe) {
+    var del=(cur-aim)*spe;
+    return aim+del;
+}
+//设置转动方法，三个参数 目标数值，当前数值，变化率%，返回（当前数值+差值*百分比变化率）
+function lerpA(aim,cur,spe) {
+    var del=cur-aim;
+    while(del>Math.PI){
+        del=del-2*Math.PI;
+    }
+    while(del<-Math.PI){
+        del=del+2*Math.PI;
+    }
+    return aim+del*spe;
+}
--- js/js.js
+++ js/js.js
@@ -15,17 +15,22 @@ var ane;//海葵
 var fruit;//果实
 var bgPic=new Image();
 
-var bigfish;
+var bigfish;//大鱼
+
+var mx;//获取鼠标 坐标X
+var my;//获取鼠标 坐标Y
 document.body.onload=game;
 function game() {
     init();
     gameloop();
 }
 function init() {
-     can1=document.getElementById("canvas1");
+     can1=document.getElementById("canvas1");//fish.dust,UI,CIRCLE
      ctx1=can1.getContext("2d");
-     can2=document.getElementById("canvas2");
+     can2=document.getElementById("canvas2");//bg,ane,fruit
      ctx2=can2.getContext("2d");
+
+     can1.addEventListener('mousemove',onMouseMove,false);
 
      bgPic.src="./src/background.jpg";
 
@@ -40,6 +45,9 @@ function init() {
 
     bigfish=new bigFishObj();
     bigfish.init();
+
+    mx=canWidth*0.5;
+    my=canHeight*0.5;
 };
 function gameloop() {
     // 根据PC性能决定帧数
@@ -54,5 +62,15 @@ function gameloop() {
     //console.log(deltaTime)
     fruit.draw();
     ctx1.clearRect(0,0,canWidth,canHeight);
+
     bigfish.draw();
+};
+function onMouseMove(e) {
+    if(e.offsetX||e.layerX)
+    {
+        mx= e.offsetX == undefined ? e.layerX:e.offsetX;
+        my= e.offsetY == undefined ? e.layerY:e.offsetY;
+        //console.log(mx);
+    }
+
 }
